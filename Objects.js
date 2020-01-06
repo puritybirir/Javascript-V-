8241e90dcd81d8f89c82e15a2f5b006a03d5b8f3
puritybirir <puritybirir@gmail.com>
2020-01-06 15:58:56 +0300

    Understanding objects and the dot notation

---
+++ Objects.js
@@ -0,0 +1,24 @@
+var person = {};
+// Assignment with dots
+person.name = "Mrs.White";
+var who = person.name;
+
+person.name = "Mr White"
+
+
+console.log (person);
+console.log(person.name)
+
+var someone = {
+    name: "Mrs.White"
+}
+
+console.log(someone)
+
+// Primitive value is a string, number, boolean, null
+// Primitive values get passed by value. Non primitive values get passed by reference
+
+
+/** 
+ * Grasping by value and by reference
+ */
