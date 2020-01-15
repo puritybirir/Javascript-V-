1c4f76cf0f1093ecbb0c764d124fcdc08274522a
puritybirir <puritybirir@gmail.com>
2020-01-15 16:34:49 +0300

    Property access using brackets

---
+++ Brackets.js
@@ -0,0 +1,27 @@
+// We use brackets when we cannot use the dot. We can only use the dot when the value is not a string literal
+
+var person = [];
+person.name = "Mrs.White";
+
+var who = person.name;
+
+
+person [0] = "I was not in the Billiards room";
+
+var persons = [];
+var plea = "wouldShe"
+persons.name = "Mrs. White";
+
+persons[plea] = "I would never"
+
+
+// Invalid characters
+
+
+x = {}
+x[0] = true;
+
+// To check if something is an array
+Array.isArray([]);
+
+
